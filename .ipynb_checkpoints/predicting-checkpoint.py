4a282cf0be0afc62bdea9fa8bf7903fd097db161
Cody Falkosky <codyfalkosky@gmail.com>
2024-03-05 17:49:09 -0800

    to_video function added

--- .ipynb_checkpoints/predicting-checkpoint.py
+++ .ipynb_checkpoints/predicting-checkpoint.py
@@ -2,8 +2,10 @@ from .boxes import Boxes
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from matplotlib.animation import FuncAnimation
 
 
+# +
 class Predicting:
     def __init__(self, parent_obj):
         self.parent_obj = parent_obj
@@ -68,14 +70,56 @@ class Predicting:
         plt.savefig(save_dir, bbox_inches='tight', pad_inches=0.1)
         plt.close()
 
-    def predict_draw_save_video(self, image_paths, save_dir):
+    def output_nms(self, image, out):
+        h, w, c = image.shape
+        
+        boxes  = out[:, :, :, 1:5]
+        boxes  = tf.reshape(boxes, (-1, 4))
+        scores = out[:, :, :, 0:1]
+        scores = tf.reshape(scores, (-1,))
+        cls    = out[:, :, :, 5:]
+        cls    = tf.reshape(cls, (-1, 2))
+        cls    = tf.argmax(cls, axis=1)
+        
+        indices = tf.image.non_max_suppression(Boxes.convert(boxes, mode='cxcywh_xyxy'), scores, max_output_size=30, score_threshold=.6)
+        
+        boxes  = tf.gather(boxes,  indices, axis=0)
+        boxes  = Boxes.scale(boxes, w/13, h/13)
+        boxes  = Boxes.convert(boxes, mode='cxcywh_xmymwh')
+        scores = tf.gather(scores, indices, axis=0)
+        cls    = tf.gather(cls, indices, axis=0)
+
+        return {'scores':scores, 'boxes':boxes, 'cls':cls}
+
+    def predict_draw_save_video(self, image_paths, save_path):
         batched = self.batch_images_to_batch_tensor(image_paths)
         out     = self.parent_obj(batched)
+        batch   = range(0, len(out))
 
-        batch = range(len(out))
-        for b in batch:
-            img = tf.io.read_file(image_paths[b])
+        fig, ax = plt.subplots(figsize=(19, 10))
+        
+        def update(frame):
+            ax.clear()
+            img = tf.io.read_file(image_paths[frame])
             img = tf.io.decode_image(img)
-            self.draw_and_save(img, out[b], f'{save_dir}/{b}.jpg')
+            ax.imshow(img)
+
+            s_b_c = self.output_nms(img, out[frame])
+
+            for (x, y, w, h), cl in zip(s_b_c['boxes'], s_b_c['cls']):
+                color = self.colors[cl.numpy()]
+                rect = Rectangle([x,y], w, h, fill=False, edgecolor=color, lw=4)
+                ax.add_patch(rect)
+                ax.axis('off')
+
+            return ax
+
+        ani = FuncAnimation(fig, update, frames=batch)
+
+        ani.save(save_path, fps=15)
+        
+
+
+# -
 
 
