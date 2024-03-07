3e59748e2aee32b83060bdd12bacc35bf3e168d6
Cody Falkosky <codyfalkosky@gmail.com>
2024-03-07 10:32:04 -0800

    fixed whitespace error

--- .ipynb_checkpoints/predicting-checkpoint.py
+++ .ipynb_checkpoints/predicting-checkpoint.py
@@ -38,38 +38,6 @@ class Predicting:
             img = tf.io.decode_image(img)
             self.draw_and_save(img, out[b], f'{save_dir}/{b}.jpg')
 
-    def draw_and_save(self, image, out, save_dir):
-        h, w, c = image.shape
-        
-        boxes  = out[:, :, :, 1:5]
-        boxes  = tf.reshape(boxes, (-1, 4))
-        scores = out[:, :, :, 0:1]
-        scores = tf.reshape(scores, (-1,))
-        cls    = out[:, :, :, 5:]
-        cls    = tf.reshape(cls, (-1, 2))
-        cls    = tf.argmax(cls, axis=1)
-        
-        indices = tf.image.non_max_suppression(Boxes.convert(boxes, mode='cxcywh_xyxy'), scores, max_output_size=30, score_threshold=.6)
-        
-        boxes  = tf.gather(boxes,  indices, axis=0)
-        boxes  = Boxes.scale(boxes, w/13, h/13)
-        boxes  = Boxes.convert(boxes, mode='cxcywh_xmymwh')
-        scores = tf.gather(scores, indices, axis=0)
-        cls    = tf.gather(cls, indices, axis=0)
-
-        plt.figure(figsize=(19, 10))
-        ax = plt.subplot(1,1,1)
-        ax.imshow(image)
-        
-        for (x, y, w, h), cl in zip(boxes, cls):
-            color = self.colors[cl.numpy()]
-            rect = Rectangle([x,y], w, h, fill=False, edgecolor=color, lw=4)
-            ax.add_patch(rect)
-        
-        plt.axis('off')
-        plt.savefig(save_dir, bbox_inches='tight', pad_inches=0.1)
-        plt.close()
-
     def output_nms(self, image, out):
         h, w, c = image.shape
         
@@ -89,7 +57,23 @@ class Predicting:
         scores = tf.gather(scores, indices, axis=0)
         cls    = tf.gather(cls, indices, axis=0)
 
-        return {'scores':scores, 'boxes':boxes, 'cls':cls}
+        return scores, boxes, cls
+
+    def draw_and_save(self, image, out, save_dir):
+        scores, boxes, cls = self.output_nms(image, out)
+
+        plt.figure(figsize=(19, 10))
+        ax = plt.subplot(1,1,1)
+        ax.imshow(image)
+        
+        for (x, y, w, h), cl in zip(boxes, cls):
+            color = self.colors[cl.numpy()]
+            rect = Rectangle([x,y], w, h, fill=False, edgecolor=color, lw=4)
+            ax.add_patch(rect)
+        
+        plt.axis('off')
+        plt.savefig(save_dir, bbox_inches='tight', pad_inches=0.1)
+        plt.close()
 
     def predict_draw_save_video(self, image_paths, save_path):
         batched = self.batch_images_to_batch_tensor(image_paths)
@@ -97,6 +81,8 @@ class Predicting:
         batch   = range(0, len(out))
 
         fig, ax = plt.subplots(figsize=(19, 10))
+        ax.axis('off')
+        fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
         
         def update(frame):
             ax.clear()
@@ -104,23 +90,18 @@ class Predicting:
             img = tf.io.decode_image(img)
             ax.imshow(img)
 
-            s_b_c = self.output_nms(img, out[frame])
+            scores, boxes, cls = self.output_nms(img, out[frame])
 
-            for (x, y, w, h), cl in zip(s_b_c['boxes'], s_b_c['cls']):
+            for (x, y, w, h), cl in zip(boxes, cls):
                 color = self.colors[cl.numpy()]
                 rect = Rectangle([x,y], w, h, fill=False, edgecolor=color, lw=4)
                 ax.add_patch(rect)
-                ax.axis('off')
-                fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
 
             return ax
 
         ani = FuncAnimation(fig, update, frames=batch)
 
         ani.save(save_path, fps=15)
+        plt.close()
         
-
-
-# -
-
 
