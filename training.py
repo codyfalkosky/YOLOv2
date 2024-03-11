a59a0a8a98d27192ccf1494737506ef86ca99bd0
Cody Falkosky <codyfalkosky@gmail.com>
2024-03-11 12:46:36 -0700

    debug valid_loss 1.31

--- training.py
+++ training.py
@@ -172,7 +172,7 @@ class Training:
             for batch in tqdm(self.parent_obj.valid_dataset, total=valid_len):
                 b_len       = len(batch['image'])
                 loss        = self.valid_step(batch)
-                last_valid.append([loss.numpy(), b_len]
+                last_valid.append([loss.numpy(), b_len])
                 self.valid_metric.update_state([loss], sample_weight=[b_len])
     
             # append training loss and reset
